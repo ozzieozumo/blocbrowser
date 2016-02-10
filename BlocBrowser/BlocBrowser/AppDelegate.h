c31c8b64a610e340664d58c294c22530a21c0f87
ozzieozumo <luke.d.everett@gmail.com>
2016-02-09 21:49:59 -0500

    complete cp23 blocbrowser webview

---
+++ BlocBrowser/BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Luke Everett on 2/6/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
